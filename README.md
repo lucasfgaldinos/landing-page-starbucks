cc3576b9939fd01ddcd2c12b5552bdb3ee102ae6
lucasfgaldinos <lucasfgaldinos@outlook.com>
2024-07-29 12:47:43 -0300

    First commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Landing Page - Starbucks</h1>
+<h2>Aplication link <a href="https://lucasfgs14.github.io/starbucks/">here</a></h2>
+<img src="./assets/page.png" />
---
+++ script.js
@@ -0,0 +1,17 @@
+const circle = document.querySelector(".circle")
+const option = document.querySelector(".optionImg")
+
+function circleOption2() {
+    option.src = "./assets/option2.png"
+    circle.style.backgroundColor = "rgb(235, 116, 149)"
+}
+
+function circleOption3() {
+    option.src = "./assets/option3.png"
+    circle.style.backgroundColor = "rgb(212, 35, 157)"
+}
+
+function circleOption1() {
+    option.src = "./assets/option1.png"
+    circle.style.backgroundColor = "#017143"
+}
